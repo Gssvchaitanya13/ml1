d8442fedb61a18249a16716afdf8fdd514d38cf3
Gssvchaitanya13 <ssvgssv13@gmail.com>
2022-07-07 16:12:53 +0530

    folder structure completed 25th june 2022

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from distutils.log import debug
 from flask import Flask
-
+from housing.logger import logging
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("we are running this code")
+
     return "ML projects started"
 
 
